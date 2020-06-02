fdef2260e9f4ebf81a6c148ddb707e881c1634ca
RobbyRabbitman <robby.rabbitman@gmail.com>
2020-06-02 20:29:00 +0200

    components -> modules removed + Material Module added

--- src/app/pages/launches/launches.module.ts
+++ src/app/pages/launches/launches.module.ts
@@ -4,11 +4,17 @@ import { CommonModule } from '@angular/common';
 import { LaunchesRoutingModule } from './launches-routing.module';
 import { LaunchesComponent } from './launches.component';
 import { LaunchesService } from 'src/app/shared/services/launches.service';
-import { LaunchesOverviewModule } from 'src/app/shared/components/launches-overview/launches-overview.module';
+import { LaunchDetailComponent } from 'src/app/shared/components/launch-detail/launch-detail.component';
+import { MaterialModule } from 'src/app/shared/material/material.module';
+import { LaunchesOverviewComponent } from 'src/app/shared/components/launches-overview/launches-overview.component';
 
 @NgModule({
-  declarations: [LaunchesComponent],
-  imports: [CommonModule, LaunchesRoutingModule, LaunchesOverviewModule],
+  declarations: [
+    LaunchesComponent,
+    LaunchDetailComponent,
+    LaunchesOverviewComponent,
+  ],
+  imports: [CommonModule, LaunchesRoutingModule, MaterialModule],
   providers: [LaunchesService],
 })
 export class LaunchesModule {}
