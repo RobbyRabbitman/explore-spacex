4dd09d792ea737fe702f1c2729c91abc4e5017fc
RobbyRabbitman <robby.rabbitman@gmail.com>
2020-06-02 19:07:20 +0200

    Launch Overview Set Up

--- src/app/shared/components/launches-overview/launches-overview.module.ts
+++ src/app/shared/components/launches-overview/launches-overview.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LaunchesOverviewComponent } from './launches-overview.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   exports: [LaunchesOverviewComponent],
   declarations: [LaunchesOverviewComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterModule,
+  ],
 })
 export class LaunchesOverviewModule {}
