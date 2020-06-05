a116ed4909d0742546ae62b327c439a78f9a60f8
RobbyRabbitman <robby.rabbitman@gmail.com>
2020-06-05 16:53:14 +0200

    basic launch detail

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  exports: [MatCardModule, MatButtonModule, MatIconModule],
+  exports: [MatCardModule, MatButtonModule, MatIconModule, MatToolbarModule],
 })
 export class MaterialModule {}
