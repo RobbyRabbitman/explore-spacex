008ff0ad98b087b40e08148423faecbaeb18821c
RobbyRabbitman <robby.rabbitman@gmail.com>
2020-06-07 21:12:13 +0200

    launch year headers

--- src/app/pages/launches/launches.component.ts
+++ src/app/pages/launches/launches.component.ts
@@ -2,6 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { LaunchesService } from 'src/app/shared/services/launches.service';
 import { Observable } from 'rxjs';
 import { Launch } from 'src/app/shared/model/launch';
+import { map } from 'rxjs/operators';
+
+interface LaunchesPerAnno {
+  launches: Launch[];
+  year: number;
+}
 
 @Component({
   selector: 'app-launches',
@@ -9,13 +15,41 @@ import { Launch } from 'src/app/shared/model/launch';
   styleUrls: ['./launches.component.scss'],
 })
 export class LaunchesComponent implements OnInit {
-  launches$: Observable<Launch[]>;
+  launches$: Observable<LaunchesPerAnno[]>;
 
   constructor(private launchesService: LaunchesService) {}
 
   ngOnInit(): void {
-    this.launches$ = this.launchesService.sortByLaunchDate(
-      this.launchesService.launches$
+    this.launches$ = this.launchesService.launches$.pipe(
+      map((launches) => {
+        let latest = Number(
+          launches.reduce((a, b) =>
+            Number(a.launch_year) < Number(b.launch_year) ? b : a
+          ).launch_year
+        );
+        let oldest = Number(
+          launches.reduce((a, b) =>
+            Number(a.launch_year) > Number(b.launch_year) ? b : a
+          ).launch_year
+        );
+        let out: LaunchesPerAnno[] = [];
+        for (let currentYear = latest; currentYear >= oldest; currentYear--) {
+          out.push({
+            launches: launches
+              .filter(
+                (launches) => Number(launches.launch_year) === currentYear
+              )
+              .sort((a, b) =>
+                new Date(a.launch_date_utc).getTime() <
+                new Date(b.launch_date_utc).getTime()
+                  ? 1
+                  : -1
+              ),
+            year: currentYear,
+          });
+        }
+        return out;
+      })
     );
   }
 }
