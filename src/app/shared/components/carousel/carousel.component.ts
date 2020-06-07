2a6414376cc2a05bee20de2a8f5b47e6bcf3e092
RobbyRabbitman <robby.rabbitman@gmail.com>
2020-06-07 19:49:23 +0200

    carousel -> config class

--- src/app/shared/components/carousel/carousel.component.ts
+++ src/app/shared/components/carousel/carousel.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
+import { Config } from './types';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.scss'],
 })
-export class CarouselComponent {
-  @Input() images: string[];
-  @Input() selected: number = 0;
-  @Input() height: string = '500px';
+export class CarouselComponent implements OnChanges {
+  @Input() config: Config;
 
   constructor() {}
+
+  ngOnChanges() {
+    console.log(this.config);
+  }
 }
---
+++ src/app/shared/components/carousel/types.ts
@@ -0,0 +1,7 @@
+export class Config {
+  constructor(
+    public images: string[],
+    public selected: number = 0,
+    public height: string = '500px'
+  ) {}
+}
