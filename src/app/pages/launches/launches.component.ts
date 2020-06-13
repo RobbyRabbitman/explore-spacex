dccfeb410d754ec6518d24b67ef11f4591b7f6b3
RobbyRabbitman <robby.rabbitman@gmail.com>
2020-06-13 20:42:47 +0200

    launches toc -> current year -> primary color

--- src/app/pages/launches/launches.component.ts
+++ src/app/pages/launches/launches.component.ts
@@ -1,10 +1,19 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  ViewChildren,
+  QueryList,
+} from '@angular/core';
 import { LaunchesService } from 'src/app/shared/services/launches.service';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { Launch } from 'src/app/shared/model/launch';
-import { map, share, take, tap } from 'rxjs/operators';
-import { Router, ActivatedRoute } from '@angular/router';
-import { ViewportScroller, DOCUMENT } from '@angular/common';
+import { map, share, take, tap, filter, scan, last } from 'rxjs/operators';
+import { isNonNull } from 'src/app/shared/utils/isNonNull';
+import { LaunchesOverviewComponent } from 'src/app/shared/components/launches-overview/launches-overview.component';
+import { element } from 'protractor';
+import { threadId } from 'worker_threads';
 
 @Component({
   selector: 'app-launches',
@@ -13,10 +22,37 @@ import { ViewportScroller, DOCUMENT } from '@angular/common';
 })
 export class LaunchesComponent implements OnInit {
   launches$: Observable<Map<number, Launch[]>>;
+  years: number[];
+
+  @ViewChildren('launch', { read: ElementRef }) set launches(
+    components: QueryList<ElementRef>
+  ) {
+    if (components.length === 0) return;
+
+    // toc elementRefs
+    const toc = this.years.map((year) =>
+      document.querySelector(`#toc_year_${year}`)
+    );
+    // create Observer
+    const observer = new IntersectionObserver((entries, observer) => {
+      let years: number[] = entries
+        .filter((entry) => entry.isIntersecting)
+        .map((entry) => Number(entry.target.id.replace('year_', '')));
+
+      if (years.length > 0) {
+        toc.forEach((element) => element.classList.remove('toc-active'));
+        let id: string = `toc_year_${years.reduce((a, b) => (a > b ? a : b))}`;
+        toc.find((element) => element.id === id).classList.add('toc-active');
+      }
+    });
+    // observe elements
+    components.forEach((element) => observer.observe(element.nativeElement));
+  }
 
   constructor(private launchesService: LaunchesService) {}
 
   ngOnInit(): void {
+    //
     this.launches$ = this.launchesService.launches$.pipe(
       take(1),
       map((launches) => {
@@ -48,6 +84,7 @@ export class LaunchesComponent implements OnInit {
         }
         return out;
       }),
+      tap((map) => (this.years = [...map.keys()])),
       share()
     );
   }
