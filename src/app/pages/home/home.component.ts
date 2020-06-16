c629c6754fe308cdc30b01a6e0044d751cc6867f
RobbyRabbitman <robby.rabbitman@gmail.com>
2020-06-16 23:33:20 +0200

    route corrected

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { LAUNCHES_BASE } from 'src/app/shared/routes/constants';
+import { LAUNCHES_BASE, ROCKETS_BASE } from 'src/app/shared/routes/constants';
 import { AssetsService } from 'src/app/shared/services/assets.service';
 import { LaunchesService } from 'src/app/shared/services/launches.service';
 import { Observable } from 'rxjs';
 import { Launch } from 'src/app/shared/model/launch';
 import { RocketService } from 'src/app/shared/services/rocket.service';
 import { Rocket } from 'src/app/shared/model/rocket';
-import { RoutingService } from 'src/app/shared/routes/routing.service';
 import { AppService } from 'src/app/shared/services/app.service';
 
 @Component({
@@ -16,6 +15,7 @@ import { AppService } from 'src/app/shared/services/app.service';
 })
 export class HomeComponent implements OnInit {
   launchesPage: string;
+  rocketsPage: string;
   latestLaunch$: Observable<Launch>;
   rocket$: Observable<Rocket>;
 
@@ -28,6 +28,7 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.launchesPage = LAUNCHES_BASE;
+    this.rocketsPage = ROCKETS_BASE;
     this.latestLaunch$ = this.launches.getLatestLaunch();
     this.rocket$ = this.rockets.getRocket('falcon9');
   }
