416d27ff6efff9b09aa5520d5f0391d9a479d5a6
RobbyRabbitman <robby.rabbitman@gmail.com>
2020-06-16 23:33:42 +0200

    rocket images added

--- src/app/shared/components/rocket-overview/rocket-overview.component.ts
+++ src/app/shared/components/rocket-overview/rocket-overview.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Rocket } from '../../model/rocket';
 import { RoutingService } from '../../routes/routing.service';
+import { AssetsService } from '../../services/assets.service';
 
 @Component({
   selector: 'app-rocket-overview',
   templateUrl: './rocket-overview.component.html',
   styleUrls: ['./rocket-overview.component.scss'],
 })
-export class RocketOverviewComponent implements OnInit {
+export class RocketOverviewComponent {
   @Input() rocket: Rocket;
 
-  constructor(public readonly ROUTES: RoutingService) {}
-
-  ngOnInit(): void {}
+  constructor(
+    public readonly ROUTES: RoutingService,
+    public assets: AssetsService
+  ) {}
 }
--- src/app/shared/services/assets.service.ts
+++ src/app/shared/services/assets.service.ts
@@ -2,7 +2,17 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class AssetsService {
-  readonly ROCKET_LAUNCH_BY_NIGHT = 'assets/images/rocket_launch_by_night.jpg';
+  public readonly FALCON_1: string = 'assets/images/rockets/falcon1.jpg';
+  public readonly FALCON_9: string = 'assets/images/rockets/falcon9.jpg';
+  public readonly FALCON_HEAVY: string =
+    'assets/images/rockets/falcon_heavy.jpg';
+  public readonly STARSHIP: string = 'assets/images/rockets/starship.jpg';
+  public readonly ROCKETS: Map<string, string> = new Map([
+    ['falcon1', this.FALCON_1],
+    ['falcon9', this.FALCON_9],
+    ['falconheavy', this.FALCON_HEAVY],
+    ['starship', this.STARSHIP],
+  ]);
 
   constructor() {}
 }
